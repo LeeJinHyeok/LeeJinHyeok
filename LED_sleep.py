772467c8d2ec91fa2995c983e7b4fcae0a45db39
Roy <wlsgur7765@gmail.com>
2019-02-01 06:57:27 +0900

    test

--- LED_sleep.py
+++ LED_sleep.py
@@ -1,10 +1,21 @@
-from neo import Led
-from time import sleep
+from neo import Gpio # import Gpio library
+from time import sleep # import sleep to wait for blinks
 
-led = Led()
+neo = Gpio() #create new Neo object
 
-while True:
-	led.on() # Turn Led on (red)
-	sleep(1) # wait one second
-	led.off() # Turn Led off
-	sleep(1)
+pinTwo = 2 #pin to use
+pinThree = 3
+
+neo.pinMode(pinTwo, neo.OUTPUT)# Use innerbank pin 2 and set it as output either 0 (neo.INPUT) or 1 (neo.OUTPUT)
+neo.pinMode(pinThree, neo.INPUT)# Use pin three(innerbank) and read set state to read
+
+#Blink example
+for a in range(0, 5): #Do for five times
+	neo.digitalWrite(pinTwo,neo.HIGH) #write high value to pin
+	sleep(1)# wait one second
+	neo.digitalWrite(pinTwo,neo.LOW) #write low value to pin
+	sleep(1)# wait one second
+
+#Read pin
+print ("Current pin("+str(pinThree)+") state is: "+str(neo.digitalRead(pinThree)))
+# read current value of pinThree(To succesfully read a pin it must be either pulled to ground or 3.3v, a non connected wire will not work)
