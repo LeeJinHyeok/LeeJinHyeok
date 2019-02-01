9b029f58aee0b23182db9275546465cda4bc36c7
Roy <wlsgur7765@gmail.com>
2019-02-02 03:15:18 +0900

    test

--- homework.py
+++ homework.py
@@ -1,14 +1,8 @@
-from datetime import datetime
-today=datetime.today()
-print(datetime.today().strftime("%Y/%m/%d"))
 
-from datetime import datetime
-today=datetime.today()
-print("Today is" + ' ' +datetime.today().strftime("%Y/%m/%d"))
-
-import time
-print("Today is {}/{}/{}".format(time.localtime().tm_year,time.localtime().tm_mon,time.localtime().tm_mday))
 
 name= input("Enter your name:")
 
-print(name)
+
+from datetime import datetime
+today=datetime.today()
+print("Hello,"+name + " Today is " + datetime.today().strftime("%Y/%m/%d"))
