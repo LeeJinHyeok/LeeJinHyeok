ee9bb4962311d4c1932582f40fc87c617a6a12fe
Roy <wlsgur7765@gmail.com>
2019-02-13 08:53:39 +0900

    test

--- gossip_bt_server.py
+++ gossip_bt_server.py
@@ -63,8 +63,8 @@
 from time import sleep
 while True:
     raw = int(open("/sys/bus/iio/devices/iio:device0/in_voltage0_raw").read())
-    v = 5 * 0.000244140625 * raw
-    t = (1000 * v) -510
+    v = raw *(3300/1024)
+    t = (v-500)/10
     sleep(1)
     print (t)
 
