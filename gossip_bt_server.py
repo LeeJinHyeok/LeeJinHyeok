b6bb962e7d64102b44ef5d0ba51709c54e200823
Roy <wlsgur7765@gmail.com>
2019-02-13 08:14:42 +0900

    test

--- gossip_bt_server.py
+++ gossip_bt_server.py
@@ -59,11 +59,16 @@
 #
 #             # Sleep for 3 seconds
 #         sleep(3)
+#
+# from time import sleep
+# while True:
+#     raw = int(open("/sys/bus/iio/devices/iio:device0/in_voltage0_raw").read())
+#     v = 5 * 0.000244140625 * raw
+#     t = (1000 * v) -510
+#     sleep(1)
+#     print (t)
 
-from time import sleep
-while True:
-    raw = int(open("/sys/bus/iio/devices/iio:device0/in_voltage0_raw").read())
-    v = 5 * 0.000244140625 * raw
-    t = (1000 * v) -510
-    sleep(1)
-    print (t)
+raw = int(open("/sys/bus/iio/devices/iio:device0/in_voltage0_raw").read())
+scale = float(open("/sys/bus/iio/devices/iio:device0/in_voltage_scale").read())
+
+print (raw*scale)
