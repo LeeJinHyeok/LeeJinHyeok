a7e93ecdb5540289a1bcba0c58e54886e43321a6
Roy <wlsgur7765@gmail.com>
2019-02-14 09:15:06 +0900

    use scale

--- temp.py
+++ temp.py
@@ -1,8 +1,9 @@
 from time import sleep
 while True:
     raw = int(open("/sys/bus/iio/devices/iio:device0/in_voltage0_raw").read())
-    v =  raw*(3300/4096)
-    t =(v-500)/10
+    scale = float(open("/sys/bus/iio/devices/iio:device0/in_voltage_scale").read())
+    v = raw * scale
+    t = (v - 500)/10
     sleep(1)
     print (t)
-    print(raw)
+    print (raw)
