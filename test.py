fe726ec5fb336a458d115c04dbbfb8a59e6280fc
Roy <wlsgur7765@gmail.com>
2019-02-20 03:33:41 +0900

    use scale

--- test.py
+++ test.py
@@ -25,7 +25,7 @@ while True:
     raw = int(open("/sys/bus/iio/devices/iio:device0/in_voltage0_raw").read())
     scale = float(open("/sys/bus/iio/devices/iio:device0/in_voltage_scale").read())
     v = raw * scale
-    t = (v - 500)/10 - 6
+    t = (v - 560)/10
     sleep(1)
     print(t)
 
