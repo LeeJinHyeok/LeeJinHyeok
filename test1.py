555029f9fdd1fff31cbb44d3f30fd8d0bae536b8
Roy <wlsgur7765@gmail.com>
2019-02-21 06:59:11 +0900

    use scale

--- test1.py
+++ test1.py
@@ -3,8 +3,6 @@ from time import sleep  # import sleep to wait for blinks
 
 
 
-
-
 while True:
 
     raw = int(open("/sys/bus/iio/devices/iio:device0/in_voltage0_raw").read())
@@ -13,4 +11,5 @@ while True:
     t = (v - 590)/10
 
     temp = (t * 1.8) + 32
+    sleep(1)
     print(t)
