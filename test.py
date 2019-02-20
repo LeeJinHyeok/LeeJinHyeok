91fe58ad87f8e3006c96bb7e5e128576396d021e
Roy <wlsgur7765@gmail.com>
2019-02-21 07:34:56 +0900

    use scale

--- test.py
+++ test.py
@@ -147,6 +147,24 @@ while True:
         c11, 2) + 2211.8 * (c11) + 7.9623)
     PM25 = 0.518 + (0.00274 * hppcf)
     print(PM25)
+
+    # AQI Convesion for NO2_SN1
+    if SN1 >= 0 and SN1 <= 53:
+        AQI_NO2 = ((50 - 0) * (SN1 - 0)) / (53 - 0) + 0
+    elif SN1 >= 54 and SN1 <= 100:
+        AQI_NO2 = ((100 - 51) * (SN1 - 54)) / (100 - 54) + 51
+    elif SN1 >= 101 and SN1 <= 360:
+        AQI_NO2 = ((150 - 101) * (SN1 - 101)) / (360 - 101) + 101
+    elif SN1 >= 361 and SN1 <= 649:
+        AQI_NO2 = ((200 - 151) * (SN1 - 361)) / (649 - 361) + 151
+    elif SN1 >= 650 and SN1 <= 1249:
+        AQI_NO2 = ((300 - 201) * (SN1 - 650)) / (1249 - 650) + 201
+    elif SN1 >= 1250 and SN1 <= 1649:
+        AQI_NO2 = ((400 - 301) * (SN1 - 1250)) / (1649 - 1250) + 301
+    elif SN1 >= 1650 and SN1 <= 2049:
+        AQI_NO2 = ((500 - 401) * (SN1 - 1650)) / (2049 - 1650) + 401
+    else:
+        AQI_NO2 = 500
     print('')
 
 #     neo.digitalWrite(pinNum[0], 0)
