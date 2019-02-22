0ae960503dcbf4bc0e34f8b0aaaea9aaafcf4b29
Roy <wlsgur7765@gmail.com>
2019-02-22 14:52:22 +0900

    test

--- gossip_bt_server.py
+++ gossip_bt_server.py
@@ -13,7 +13,7 @@ from neo import Gpio
 #array for calculate alph
 #temp              -30,  -20   -10     0    10     20   30    40    50
 #index               0,    1,    2,    3,    4,    5,    6,    7 ,   8
-O3_tempArray  = [ 0.18, 0.18, 0.18, 0.18, 0.18, 0.18, 0.18, 0.18, 2.87]
+O3_tempArray  = [ 0.18, 0.18, 0.18, 0.18, 0.18, 0.36, 0.76, 1.56, 2.87]
 SO2_tempArray = [ 0.85, 0.85, 0.85, 0.85, 0.85, 1.15, 1.45, 1.75, 1.95]
 NO2_tempArray = [ 1.18, 1.18, 1.18, 1.18, 1.18, 1.18, 1.18, 2.00, 2.70]
 CO_tempArray  = [ 1.40, 1.03, 0.85, 0.62, 0.30, 0.03,-0.25,-0.48,-0.80]
@@ -239,7 +239,7 @@ if __name__ == '__main__':
             raw = int(open("/sys/bus/iio/devices/iio:device0/in_voltage0_raw").read())
             scale = float(open("/sys/bus/iio/devices/iio:device0/in_voltage_scale").read())
             c0 = raw * scale
-            temp_celsius = (c0-600) / 10
+            temp_celsius = (c0-600) / 10 -10
             temp = (temp_celsius * 1.8) +32
             temp = int (temp)
 
