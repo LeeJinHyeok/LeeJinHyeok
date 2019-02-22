3f0a71674e8bdbe4751531bc58ab6ed139b18b2a
Roy <wlsgur7765@gmail.com>
2019-02-22 16:51:26 +0900

    test

--- test.py
+++ test.py
@@ -206,6 +206,7 @@ while True:
     raw = int(open("/sys/bus/iio/devices/iio:device0/in_voltage0_raw").read())
     scale = float(open("/sys/bus/iio/devices/iio:device0/in_voltage_scale").read())
     c0 = raw * scale
+    temp_celsius = (c0 - 600) / 10 - 10
     if 12 <= temp_celsius < 15:
         temp_celsius = temp_celsius + 10
     elif 15 <= temp_celsius < 18:
